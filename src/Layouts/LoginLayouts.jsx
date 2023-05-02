0777ebdec0eacdba770a70758cf5a6c9a12718ba
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-02 13:42:58 +0600

    footer header added on login layout

--- src/Layouts/LoginLayouts.jsx
+++ src/Layouts/LoginLayouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Shared/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 
 const LoginLayouts = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
 
         </div>
     );
