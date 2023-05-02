bae9a6043ddc7e86a553106f4fe0557e099a28e6
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-02 13:56:18 +0600

    login form added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Please Registration !</h1>
+                    <h1 className="text-5xl font-bold">Please Register !</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -38,7 +38,7 @@ const Register = () => {
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Register</button>
                         </div>
                     </form>
                 </div>
