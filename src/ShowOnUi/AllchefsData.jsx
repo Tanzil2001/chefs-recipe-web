8c408bb26fd10d2bc17ba1e69fe142811eacc35b
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-04 15:55:31 +0600

    responsive modify

--- src/ShowOnUi/AllchefsData.jsx
+++ src/ShowOnUi/AllchefsData.jsx
@@ -6,9 +6,8 @@ import LazyLoad from 'react-lazy-load';
 const AllchefsData = ({ allChefs }) => {
     const { id, chef_name, chef_picture, likes, num_recipes, years_of_experience } = allChefs;
     return (
-        <div>
             <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto mt-5">
-                <figure><LazyLoad width={1024} height={300} threshold={1}><img src={chef_picture} alt="Shoes" /></LazyLoad></figure>
+                <figure><LazyLoad width={500} height={300} threshold={1}><img src={chef_picture} alt="Shoes" /></LazyLoad></figure>
                 <div className="card-body">
                     <h2 className="card-title">{chef_name}</h2>
                     <p>Number of Recipes: {num_recipes}</p>
@@ -19,8 +18,6 @@ const AllchefsData = ({ allChefs }) => {
                     </div>
                 </div>
             </div>
-        </div>
-
     );
 };
 
